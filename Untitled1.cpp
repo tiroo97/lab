2309eadf754f28273c9dc34dedf2e468b5a2ee8c
tiroo97 <tiroo97@gmail.com>
2017-03-13 21:43:37 +0200

    program1

---
+++ Untitled1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double side;
+    cout<<"Enter side:";
+    cin>>side;
+    cout<<"\Area = "<<side*side<<"\nPerimeter = "<<side*4;
+    return 0;
+}
